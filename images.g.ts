9c6498ed96129f183525b19ce9906a95dad0c5b3
Eric Caron <eric.caron@gmail.com>
2023-11-19 16:43:58 -0600

    Merged momo level

--- images.g.ts
+++ images.g.ts
@@ -22,6 +22,272 @@ namespace myImages {
 . . . . . . c c c c c c c c c . 
 . . . . . . c 4 4 c c c 4 4 c . 
 `;
+            case "image3":
+            case "instrument3":return img`
+. . . c c c c c c c c c c c c . 
+. . . f f f f f f f f f f c c . 
+. . . f e e e e e e e e f c c . 
+. . . f e f e e e e f e f c c . 
+. . . f e e e e e e e e f c c . 
+. . . f e e e e e e e e f c c . 
+. . . f e e e f f e e e f c c . 
+. . . f e e f b b f e e f c c . 
+. . . f e f b c c b f e f c c . 
+. . . f e f b c c b f e f c c . 
+. . . f e e f b b f e e f c c . 
+. . . f e e e f f e e e f c c . 
+. . . f e e e e e e e e f c c . 
+. . . f e f e e e e f e f c c . 
+. . . f e e e e e e e e f c c . 
+. . . f f f f f f f f f f c . . 
+`;
+            case "image4":
+            case "instrument2":return img`
+. . . . . . . . . . . . . f f f 
+. . . . . . . . . . . . f e 4 e 
+. . . . . . . . . . . . f 4 4 e 
+. . . . . . . . . . . f f e e . 
+. . . . . f f f e . f f e e . . 
+. . . . f f e 6 6 e f e e . . . 
+. . . . f f 6 4 4 4 f e . . . . 
+. f f f f 2 4 4 4 4 4 e . . . . 
+f f e 6 6 4 4 e f 2 4 4 e . . . 
+f f 6 4 4 4 4 f f f 4 4 e . . . 
+f e 4 4 4 e 4 2 f e 4 4 e . . . 
+e e 4 4 4 e e 4 4 4 2 e . . . . 
+. e 4 4 4 4 e e 4 4 e . . . . . 
+. . e 4 4 4 4 4 4 4 e . . . . . 
+. . . e 4 4 4 4 4 e . . . . . . 
+. . . . e e e e e . . . . . . . 
+`;
+            case "image5":
+            case "camera":return img`
+. . . c c c c . . . . . . . . . 
+. . c b b b b c c c c . . . . . 
+. c b b b b b b b b b c c c c . 
+c b b b b b b b b b b b b b 3 c 
+c c c c b b b b b b b b b 3 3 c 
+c 2 2 d b c c c b b b b 3 3 b c 
+c 2 2 d d d d b c c c 3 3 b b c 
+f b d d d d d d d d c 3 b b b c 
+f c c c f b d d d d c 3 b b b c 
+f c c b d d c f b b c 3 b b b c 
+f c b c f f d c f f c 3 b b b c 
+f c b c d f f d f f c 3 b b b c 
+f c c b c c f b f f c 3 b b c . 
+f f f c b b b f f f c 3 b c . . 
+. . . f f f f f f c c b c . . . 
+. . . . . . . f f f f f . . . . 
+`;
+            case "image6":
+            case "stack":return img`
+f f f f f f f f f f f f f f f f 
+f 1 1 d d d d d d d d d d 1 1 f 
+f 1 b b b b b b b b b b b b 1 f 
+f d b b b b b b b b b b b b d f 
+f d b b b b b b b b b b b b d f 
+f 1 b b b b b b b b b b b b 1 f 
+f 1 1 d d d d d d d d d d 1 1 f 
+f b b b b b b b b b b b b b b f 
+f b d d f c c c c c c f d d b f 
+f d d f f c c c c c c f f d d f 
+f b f f c c c c c c c c f f b f 
+f b c c c c c c c c c c c c b f 
+f b c c c c c c c c c c c c b f 
+f d f f c c c c c c c c f f d f 
+f d d f f c c c c c c f f d d f 
+f b b b f c c c c c c f b b b f 
+`;
+            case "image7":
+            case "stage":return img`
+b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b 
+b b 1 1 1 1 1 1 1 1 1 1 1 1 b b 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 1 1 7 1 7 1 1 6 6 1 1 6 1 1 8 
+8 1 6 6 1 6 1 7 1 6 1 6 6 1 7 8 
+8 6 1 6 1 6 1 7 1 6 1 1 6 1 6 8 
+8 1 1 6 1 6 1 6 1 6 7 1 6 1 1 8 
+8 6 6 6 6 6 6 6 6 6 6 6 6 6 6 8 
+8 6 6 6 6 6 6 6 6 6 6 6 6 6 6 8 
+8 6 6 8 8 8 8 8 8 8 8 8 4 4 6 8 
+8 6 6 8 6 6 6 6 6 6 6 4 5 5 4 8 
+8 6 6 8 6 6 6 6 6 6 6 8 5 5 8 8 
+8 6 6 8 6 6 6 6 6 6 6 6 8 8 6 8 
+8 6 6 7 7 7 7 7 7 7 7 7 7 6 6 8 
+8 6 6 6 6 6 6 6 6 6 6 6 6 6 6 8 
+`;
+            case "image8":
+            case "exit":return img`
+b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b 
+b b 1 1 1 1 1 1 1 1 1 1 1 1 b b 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e 2 2 2 2 2 2 2 2 2 2 2 2 2 2 e 
+e 2 1 1 2 1 2 1 2 1 2 1 1 1 2 e 
+e 2 1 3 2 3 1 3 2 1 2 2 1 2 2 e 
+e 2 1 2 2 3 1 3 2 1 2 2 1 2 2 e 
+e 2 1 1 2 1 2 1 2 1 2 2 1 2 2 e 
+e 2 2 2 2 2 2 2 2 2 2 2 e e 2 e 
+e 2 2 e e e e e e e e e 4 4 e e 
+e 2 2 e 2 2 2 2 2 2 2 4 5 5 4 e 
+e 2 2 e 2 2 2 2 2 2 2 e 5 5 e e 
+e 2 2 e 2 2 2 2 2 2 2 2 e e 2 e 
+e 2 2 3 3 3 3 3 3 3 3 3 3 2 2 e 
+e 2 2 2 2 2 2 2 2 2 2 2 2 2 2 e 
+`;
+            case "image9":
+            case "fan1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . f 1 1 1 1 1 1 1 f . . . 
+. . . . f 1 1 1 1 1 1 1 f . . . 
+. . . . f 1 1 1 1 1 1 1 f . . . 
+. . . . f 2 2 1 1 2 2 2 f . . . 
+. . . . f 1 1 1 1 1 1 1 f . . . 
+. . . . f 1 1 2 1 1 1 1 f . . . 
+. . . f f 1 1 1 1 1 1 1 f . . . 
+. . f 1 1 f f f f f f f . . . . 
+. f 1 1 1 f 1 1 1 1 f f . . . . 
+. f 1 1 f f 1 1 f 1 1 f . . . . 
+. f 1 1 f 1 1 1 f f 1 f . . . . 
+. f f f f 1 1 1 1 f f f . . . . 
+. . f 1 1 1 1 1 1 f . . . . . . 
+. . . f f f f f f f . . . . . . 
+`;
+            case "image10":
+            case "fan3":return img`
+. . . . f f f f f f f f . . . . 
+. . . f f 1 1 1 1 1 1 f f . . . 
+. . f f 1 1 1 1 1 1 1 1 f . . . 
+. . f 1 1 2 2 1 1 2 2 1 1 f . . 
+f f f f 1 1 1 1 1 1 1 1 f f . . 
+f 1 1 f f 1 1 1 1 1 1 f f . . . 
+f 1 1 1 f 1 1 1 2 1 f f f f f . 
+f f 1 1 1 1 1 1 1 f f 1 1 1 f . 
+. f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+. f f 1 1 1 1 1 1 1 1 1 f f . . 
+. . f 1 1 1 1 1 1 f f f f . . . 
+. . f 1 1 1 1 1 1 f . . . . . . 
+. . f f 1 1 1 1 1 1 f f f . . . 
+. . . f f 1 1 1 1 1 1 1 f . . . 
+. . . . f f 1 1 1 1 1 f f . . . 
+. . . . . . f f f f f . . . . . 
+`;
+            case "image11":
+            case "fan2":return img`
+. . . . . f f f f f f f f f . . 
+. . . . . f 1 1 1 1 1 1 1 f . . 
+. f f f . f 1 1 1 1 1 1 1 f . . 
+. f 1 1 f f f 2 2 1 2 2 2 f . . 
+. f 1 1 1 f f 1 1 1 1 1 1 f . . 
+. f f f 1 1 f f 1 2 1 1 f f . . 
+. . . f f f 1 1 1 1 f f f . . . 
+. . . . . f f 1 1 1 1 f f f . . 
+. . f f . . . f 1 1 1 1 1 f f . 
+. f f f . . . f 1 1 f f f 1 f f 
+. f 1 f . . f 1 1 1 f . f f f f 
+. f 1 f f f f 1 1 1 f . . . . . 
+. f 1 1 f f 1 1 1 1 f . . . . . 
+. f 1 1 1 1 1 1 1 1 f . . . . . 
+. . f 1 1 1 1 1 1 f . . . . . . 
+. . f f f f f f f . . . . . . . 
+`;
+            case "image12":
+            case "instrument0":return img`
+. . . . . b b . . b b . . . . . 
+. . . . b d b b b b d b . . . . 
+. . . . b b d 1 1 d d b e . . . 
+. . . b d 1 1 1 1 b b f c e . . 
+. b b d 1 1 d d 1 1 b f f c e . 
+b d b 1 1 d d d 1 1 b f f f c e 
+b b d 1 d d d d 1 d b 5 f f f c 
+. b 1 1 d d d 1 1 b 5 5 5 f f f 
+. b 1 1 1 1 1 1 b b c 5 5 5 f f 
+b b d b 1 1 d b b f f c f 5 f f 
+b d d b b b b 5 c f f f c f f f 
+. b b f f f 5 5 f c f f f c e . 
+. . e c f f f 5 5 f c f f f e . 
+. . . e c f f f 5 f f c f e . . 
+. . . . e c f f f f f f f . . . 
+. . . . . e c f f f f . . . . . 
+`;
+            case "image13":
+            case "instrument1":return img`
+. . . . . . . . . . . . . f 9 f 
+. . . . . . . . . . . . f e 7 e 
+. . . . . f f e e e . . 2 4 5 e 
+. . . . f f e 6 6 e . . f e e . 
+. . . . f e 6 6 6 . . f e e . . 
+. . . . e e 6 6 . . f e e . . . 
+. . . . f f 6 6 . f e e . . . . 
+. f e f f f 6 6 f e e . . . . . 
+f e e 6 6 6 6 6 9 e e e 6 e . . 
+f e 2 6 6 6 6 1 1 9 6 6 6 e . . 
+e e 6 4 c c 1 1 1 1 9 6 6 e . . 
+e e 6 6 5 d b 1 1 1 e e e . . . 
+. e 6 6 6 7 d c 1 6 e . . . . . 
+. . e 6 6 6 9 c 6 6 e . . . . . 
+. . . e 6 6 6 a 6 e . . . . . . 
+. . . . e e e e e . . . . . . . 
+`;
+            case "image14":
+            case "instrument4":return img`
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . f c f c f c f f . . . . . 
+. . . f f c f c f c f . . . . . 
+. . . f c f c f c f f . . . . . 
+. . . f f c f c f c f . . . . . 
+. . . . f 2 f f f f . . . . . . 
+. . . . f 4 6 6 6 f . . . . . . 
+. . . . f 5 6 6 b f . . . . . . 
+. . . . f 7 6 6 d f . . . . . . 
+. . . . f 9 6 6 6 f . . . . . . 
+. . . . f a 6 6 6 f . . . . . . 
+. . . . . f f f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image15":
+            case "wall":return img`
+c c f c c c 2 c c c c c c c c c 
+f f f f c 2 c c f f f f c c f c 
+c c c c 2 2 2 c c f c c c c f c 
+c c c 2 4 4 2 2 c f 2 c f f f f 
+c c f f 2 2 2 4 4 2 c c c c c c 
+c 9 b f 2 4 2 2 c c c c f c c c 
+9 6 6 6 9 2 2 c c c c c f c c c 
+9 6 6 6 6 9 f f c c f f f f f f 
+c f b 6 6 6 b f c c c c c c f c 
+c f f 9 6 6 6 6 9 c c c c c f c 
+c c c c 9 6 6 6 9 c c c c c c c 
+c c c c c f b 9 c c c c c f c c 
+c c c f c f f c c c c c c f c c 
+c c c f c c c c f f f f f f f c 
+c c c f c c c c c c c f c c c c 
+f f f f f f f c c c c f c c c c 
+`;
+            case "image2":
+            case "rocker":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . f e e e . . . . . . 
+. . . . . f e e e e e . . . . . 
+. . . . . f f e b f f f . . . . 
+. . . . . e b b e b c e . . . . 
+. . . . . b d b c d b e . . . . 
+. . . . . f e d d b e . . . . . 
+. . . . f f f b b f f f . . . . 
+. . . . f f f c c c f f f . . . 
+. . . . . f c c e c f . . . . . 
+. . . . . e c f f c e . . . . . 
+. . . . . . e 8 8 c . . . . . . 
+. . . . . . c 8 8 c . . . . . . 
+. . . . . . 6 6 6 6 . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
         }
         return null;
     })
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,28 +2,31 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16], TileScale.Sixteen);
             case "level2":
             case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
 . . . . . . . . . . . . . . . . 
@@ -81,6 +84,44 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
+            case "level5":
+            case "level5":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`10001000020202020202020202020202020205050200000000000002000000000000000203000505000209020005000200050002020005050002020200020002000200020200000005020a02000200050002000202000202020200050002000000020002020000000000000000020a0200020002020006020202020500020202000200020200000002000b00000207020002000202000202020002020202000500020002020000000000020000000000000200020200050202020502020202020402000202000000000802050505000000020002020002020202020202020202020500050204020700000000000000000000000c02020202020202020202020202020201`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . 2 . . . . . . . 2 
+. . 2 2 . 2 . 2 . . . 2 . . . 2 
+2 . 2 2 . 2 2 2 . 2 . 2 . 2 . 2 
+2 . . . 2 2 . 2 . 2 . . . 2 . 2 
+2 . 2 2 2 2 . . . 2 . . . 2 . 2 
+2 . . . . . . . . 2 . 2 . 2 . 2 
+2 . . 2 2 2 2 2 . 2 2 2 . 2 . 2 
+2 . . . 2 . 2 . . 2 . 2 . 2 . 2 
+2 . 2 2 2 . 2 2 2 2 . . . 2 . 2 
+2 . . . . . 2 . . . . . . 2 . 2 
+2 . . 2 2 2 2 2 2 2 2 2 . 2 . 2 
+2 . . . . . 2 . . . . . . 2 . 2 
+2 . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+2 . 2 . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11], TileScale.Sixteen);
         }
         return null;
     })
@@ -88,6 +129,17 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "tile1":return tile1;
+            case "tile2":return tile2;
+            case "tile3":return tile3;
+            case "tile4":return tile4;
+            case "tile5":return tile5;
+            case "tile6":return tile6;
+            case "tile7":return tile7;
+            case "tile8":return tile8;
+            case "tile9":return tile9;
+            case "tile10":return tile10;
+            case "tile11":return tile11;
         }
         return null;
     })
